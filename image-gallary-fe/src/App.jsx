5502930361e248bfaab8d7fd29332fe2a763870e
rasedul99 <rasedul2199@gmail.com>
2023-10-31 23:42:19 +0600

    initial folder structer

---
+++ image-gallary-fe/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Gallary from './Components/Gallary/Gallary'
+
+const App = () => {
+  return (
+    <div>
+      <Gallary />
+    </div>
+  )
+}
+
+export default App
