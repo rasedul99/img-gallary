201cad345da796169b26647684ad1222d87e6e20
rasedul99 <rasedul2199@gmail.com>
2023-11-02 23:04:45 +0600

    kept separate file for get images from local folder

--- image-gallary-fe/src/Components/Gallary/Gallary.jsx
+++ image-gallary-fe/src/Components/Gallary/Gallary.jsx
@@ -2,27 +2,16 @@ import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImage } from '@fortawesome/free-solid-svg-icons'
 import "./Gallary.css";
-import img1 from '../../assets/images/image-1.webp'
-import img2 from '../../assets/images/image-2.webp'
-import img3 from '../../assets/images/image-3.webp'
-import img4 from '../../assets/images/image-4.webp'
-import img5 from '../../assets/images/image-5.webp'
-import img6 from '../../assets/images/image-6.webp'
-import img7 from '../../assets/images/image-7.webp'
-import img8 from '../../assets/images/image-8.webp'
-import img9 from '../../assets/images/image-9.webp'
-import img10 from '../../assets/images/image-2.webp'
-import img11 from '../../assets/images/image-4.webp'
+import { getImages } from "../../Helper/data"
 
 const Gallary = () => {
-  const initialState = [{ id: 1, path: img1, title: "headphone" }, { id: 2, path: img2, title: "headphone" }, { id: 3, path: img3, title: "headphone" }, { id: 4, path: img4, title: "headphone" }, { id: 5, path: img5, title: "headphone" }, { id: 6, path: img6, title: "headphone" }, { id: 7, path: img7, title: "headphone" }, { id: 8, path: img8, title: "headphone" }, { id: 9, path: img9, title: "headphone" }, { id: 10, path: img10, title: "headphone" }, { id: 11, path: img11, title: "headphone" }]
 
-  const [images, setImages] = useState(initialState)
+  const [images, setImages] = useState(getImages)
   const [selectedItems, setSelectedItems] = useState([]);
   //  Handle checkboxes 
   const handleCheckBoxChange = (e) => {
     const { value, checked } = e.target;
-    
+
     // Case 1: The user checks the box
     if (checked) {
       setSelectedItems((prevSelectedItems) => [...prevSelectedItems, +value]);
@@ -38,9 +27,8 @@ const Gallary = () => {
   const handleDeleteSelectedItems = (items) => {
     const updatedImages = images.filter((image) => !items.includes(image.id));
     setImages(updatedImages);
-
     // Clear selected items after deleting
-    setSelectedItems();
+    setSelectedItems([]);
   }
   return (
     <>
---
+++ image-gallary-fe/src/Helper/data.js
@@ -0,0 +1,14 @@
+import img1 from '../assets/images/image-1.webp'
+import img2 from '../assets/images/image-2.webp'
+import img3 from '../assets/images/image-3.webp'
+import img4 from '../assets/images/image-4.webp'
+import img5 from '../assets/images/image-5.webp'
+import img6 from '../assets/images/image-6.webp'
+import img7 from '../assets/images/image-7.webp'
+import img8 from '../assets/images/image-8.webp'
+import img9 from '../assets/images/image-9.webp'
+import img10 from '../assets/images/image-2.webp'
+import img11 from '../assets/images/image-4.webp'
+
+export const getImages = [{ id: 1, path: img1, title: "headphone" }, { id: 2, path: img2, title: "headphone" }, { id: 3, path: img3, title: "headphone" }, { id: 4, path: img4, title: "headphone" }, { id: 5, path: img5, title: "headphone" }, { id: 6, path: img6, title: "headphone" }, { id: 7, path: img7, title: "headphone" }, { id: 8, path: img8, title: "headphone" }, { id: 9, path: img9, title: "headphone" }, { id: 10, path: img10, title: "headphone" }, { id: 11, path: img11, title: "headphone" }]
+
